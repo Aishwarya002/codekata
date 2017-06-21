c70bccadfacbfe9a6564e224b9bc5121a8fa76c9
Aishwarya002 <tweetyaishu.sweety@gmail.com>
2017-06-21 14:15:46 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,1 +1,10 @@
-
+count=0
+x=input("enter a number")
+for i in range(x,x/2):
+	if(x%i==0):
+		print("number is not prime")
+		count=1
+if(count==0):
+	print("prime number")
+else:
+	print("not a prime")
