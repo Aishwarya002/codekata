92b0f002ffb739512e90cde909a2105973f41fd3
Aishwarya002 <tweetyaishu.sweety@gmail.com>
2017-06-21 21:03:46 +0530

    Update prime1.py

--- prime1.py
+++ prime1.py
@@ -1,9 +1,11 @@
-l=int(input())
-h=int(input())
-for n in range(l,h+1):
-  if(n>1):
-    for i in range(2,n):
-      if(n%i)==0:
-        break
-     else:
-      print(n)
+low=input("enter a low value")
+high=input("enter a high value")
+while (low < high):
+    flag = 0
+    for i in range(2,low):
+        if(low % i == 0):
+            flag = 1
+            break
+    if (flag == 0):
+        print(low)
+        low=low+1
