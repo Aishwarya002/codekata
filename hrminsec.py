d49b5e99d67259cc7658678bc6930a468fc90289
Aishwarya002 <tweetyaishu.sweety@gmail.com>
2017-06-22 11:25:48 +0530

    Create hrminsec.py

---
+++ hrminsec.py
@@ -0,0 +1,18 @@
+hr=input("enter hr")
+min=input("enter min")
+sec=input("enter sec")
+hr1=input("enter hr")
+min1=input("enter min")
+sec1=input("enter sec")
+if(hr>hr1):
+	print(hr-hr1)
+else:
+	print(hr1-hr)
+if(min>min1):
+	print(min-min1)
+else:
+	print(min1-min)
+if(sec-sec1):
+	print(sec-sec1)
+else:
+	print(sec1-sec)
