da6b5944ab05797bd8a084379975da0f7d1df38c
Aishwarya002 <tweetyaishu.sweety@gmail.com>
2017-06-23 21:08:59 +0530

    Update amstr.py

--- amstr.py
+++ amstr.py
@@ -1,12 +1,18 @@
-x=input("enter a number")
-y=x
-result=0
+x=raw_input("enter a number")
+z=len(x)
+print(z)
+y=int(x)
+y1=int(x)
+result=1
+sum=0
 while(y!=0):
 	remin=y%10
-	result=result+remin*remin*remin
+	result=1
+	for i in range(z):
+		result=result*remin
+	sum=sum+result
 	y=y/10
-print(result)
-if(result==x):
+if(sum==y1):
 	print("amstrong")
 else:
 	print("not")
