729ba3e0239ec2147db3fb50bed004e3e2022b23
Aishwarya002 <tweetyaishu.sweety@gmail.com>
2017-06-23 10:22:18 +0530

    Update prime1.py

--- prime1.py
+++ prime1.py
@@ -1,11 +1,10 @@
-low=input("enter a low value")
+low1=input("enter a low value")
 high=input("enter a high value")
-while (low < high):
+for low in range(low1,high):
     flag = 0
     for i in range(2,low):
         if(low % i == 0):
             flag = 1
             break
     if (flag == 0):
-        print(low)
-        low=low+1
+        print(low
